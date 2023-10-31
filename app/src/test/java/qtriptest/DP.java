b8050d0bfd6aa07b60e432cd11096e35a9b2c0be
Crio.GitLab <admin@example.com>
2023-10-31 08:17:34 +0000

    Update DP.java (Nov '23 - Give learners DP Solution) (Crio.Do)

--- app/src/test/java/qtriptest/DP.java
+++ app/src/test/java/qtriptest/DP.java
@@ -2,4 +2,44 @@ package qtriptest;
 
 
 public class DP {
+    // TODO: use correct annotation to connect the Data Provider with your Test Cases
+    
+    public Object[][] dpMethod(Method m) throws IOException {
+        int rowIndex = 0;
+        int cellIndex = 0;
+        List<List> outputList = new ArrayList<List>();
+
+        FileInputStream excelFile = new FileInputStream(new File(
+                "<absolute-path-to-xlsx-file>"));
+        Workbook workbook = new XSSFWorkbook(excelFile);
+        Sheet selectedSheet = workbook.getSheet(m.getName());
+        Iterator<Row> iterator = selectedSheet.iterator();
+        while (iterator.hasNext()) {
+            Row nextRow = iterator.next();
+            Iterator<Cell> cellIterator = nextRow.cellIterator();
+            List<String> innerList = new ArrayList<String>();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                if (rowIndex > 0 && cellIndex > 0) {
+                    if (cell.getCellType() == CellType.STRING) {
+                        innerList.add(cell.getStringCellValue());
+                    } else if (cell.getCellType() == CellType.NUMERIC) {
+                        innerList.add(String.valueOf(cell.getNumericCellValue()));
+                    }
+                }
+                cellIndex = cellIndex + 1;
+            }
+            rowIndex = rowIndex + 1;
+            cellIndex = 0;
+            if (innerList.size() > 0)
+                outputList.add(innerList);
+
+        }
+
+        excelFile.close();
+
+        String[][] stringArray = outputList.stream().map(u -> u.toArray(new String[0])).toArray(String[][]::new);
+        return stringArray;
+
+    }
 }
